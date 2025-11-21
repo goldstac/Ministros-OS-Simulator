6eae292ea4bf479b8a788af86fc8dd04771cf18a
Li Productions <172457372+goldstac@users.noreply.github.com>
2025-11-21 17:11:16 +0400

    v1.1
    rewrote the code from scratch and added grub simulation added touch & ls

--- main.c
+++ main.c
@@ -1,61 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 int main() {
-    char username[20];
-    char password[30];
-    char input[100];
-    printf("Booting....\n");
+    char username[50];
+    char password[50];
+    int option_os;
+    char input[50];
+    char splash[256];
+    char touch_filename[50];
+    char touch_file[50];
+    char ls[256];
+    printf("Grub\n");
+    printf("1 : Ministros OS\n");
+    printf("Choose Operating System To Boot\n");
+    scanf("%d",&option_os);
+    getchar();
+    if (option_os == 1) {
+    sleep(1);
+    printf("Booting...\n");
     FILE *file = fopen("splash.txt","r");
-    char data[256];
-    sleep(1.5);
-    while (fgets(data,sizeof(data),file)) {
-        printf("%s",data);
+    sleep(2.3);
+    while (fgets(splash,sizeof(splash),file)) {
+    printf("%s",splash);
     }
-    printf("\n");
-     sleep(1.5);
-    printf("Ministros OS Login\n");
+    fclose(file);
+    printf("OS\n");
     printf("Username\n");
     fgets(username,sizeof(username),stdin);
     username[strcspn(username,"\n")] = '\0';
-    printf("Password \n");
+    printf("Password\n");
     fgets(password,sizeof(password),stdin);
     password[strcspn(password,"\n")] = '\0';
-    printf("Welcome %s \n",username);
-    while(1) {
-        printf("%s@ministros $ > ",username);
+    
+    while (1) {
+        printf("%s@ministros [~] $> ",username);
         fgets(input,sizeof(input),stdin);
-       input[strcspn(input,"\n")] = '\0';
-       if (strcmp(input,"whoami") == 0) {
-        printf("%s\n",username);
-       }
-       else if (strcmp(input,"--version") == 0) {
-        printf("Ministros OS v1.0\n");
-       }
-       else if (strcmp(input,"shutdown now") == 0 || strcmp(input,"exit") == 0) {
-        break;
-       }
-       else if (strcmp(input,"help") == 0) {
-        printf("whoami : prints username\n");
-        printf("--version : prints version\n");
-        printf("shutdown now : exit system\n");
-        printf("exit : exit system\n");
-        printf("help : prints commands\n");
-        printf("sudo pacman -Syu : updates system\n");
-       }
-       else if (strcmp(input,"sudo pacman -Syu") == 0) {
-        sleep(0.3);
-        printf("Updating Ministros OS ########\n");
-        sleep(1);
-        printf("Updating Ministros OS ########\n");
-        sleep(1);
-        printf("Updating Ministros OS ########\n");
-        printf("Ministros OS Updated\n");
-       }
-       else{
-        printf("Command Not Found\n");
-       }
+        input[strcspn(input,"\n")] = '\0';
+        if (strcmp(input,"whoami") == 0) {
+            printf("%s\n",username);
+        } 
+        else if (strcmp(input,"--version") == 0) {
+            printf("Ministros OS v1.2\n");
+        }
+        else if (strcmp(input,"touch") == 0) {
+            printf("Enter Filename\n");
+            fgets(touch_filename,sizeof(touch_filename),stdin);
+            touch_filename[strcspn(touch_filename,"\n")] = '\0';
+            FILE *touch = fopen("touch.txt","a");
+            fprintf(touch,"%s\n",touch_filename);
+            fclose(touch);
+        }  
+        else if (strcmp(input,"ls") == 0) {
+            FILE *touch_file = fopen("touch.txt","r");
+        while (fgets(ls,sizeof(ls),touch_file)) {
+            ls[strcspn(ls,"\n")] = '\0';
+            printf("%s\n",ls);
+        }
+        fclose(touch_file);
+      }
 
     }
+    }
+    else{
+        printf("Boot Option Not Found\n");
+    }
+    return 0;
 }
